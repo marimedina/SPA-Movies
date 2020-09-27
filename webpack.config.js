75f5f82a990ce1d380c20e2a61e5a32db5cb7f73
marimedina <mariescuela@outlook.com>
2020-09-27 19:08:50 -0300

    Modificado

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         ),
         new CopyWebpackPlugin({
             patterns: [{from:'./src/styles/styles.css',
-            to:""}]
+            to:''}]
         }),
 
         new MiniCssExtractPlugin()       
